42a5a017c6e04f3828a58930bb505c884ce2461f
Shimokawa Takahiro <takahiro.1828@gmail.com>
2015-08-05 22:25:29 +0900

    Modify list foreground color

--- src/com/androhi/androiddrawableviewer/form/ImageListCellRenderer.java
+++ src/com/androhi/androiddrawableviewer/form/ImageListCellRenderer.java
@@ -6,16 +6,24 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ImageListCellRenderer implements ListCellRenderer {
+
     @Override
     public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
         if (value instanceof JPanel) {
+            setLabelColor(((JPanel) value), isSelected);
             Component component = (Component) value;
-            component.setForeground(JBColor.white);
             Color normalBackground = index % 2 == 0 ? JBColor.border() : JBColor.background();
             component.setBackground(isSelected ? UIManager.getColor("List.selectionBackground") : normalBackground);
             return component;
         } else {
             return new JLabel("");
+        }
+    }
+
+    private void setLabelColor(JPanel panel, boolean isSelected) {
+        Component[] components = panel.getComponents();
+        for (Component cmp : components) {
+            cmp.setForeground(isSelected ? UIManager.getColor("List.selectionForeground") : JBColor.foreground());
         }
     }
 }
