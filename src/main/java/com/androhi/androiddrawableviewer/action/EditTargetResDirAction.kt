33efb8c7119df6e50b264c4027896ec5d212df3e
Shimokawa Takahiro <takahiro.1828@gmail.com>
2016-04-06 01:45:40 +0900

    Convert AnAction subclass to kotlin

---
+++ src/main/java/com/androhi/androiddrawableviewer/action/EditTargetResDirAction.kt
@@ -0,0 +1,19 @@
+package com.androhi.androiddrawableviewer.action
+
+import com.androhi.androiddrawableviewer.form.SettingsDialog
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class EditTargetResDirAction : AnAction("Edit directory", "Edit target resource directory", AllIcons.General.Settings) {
+
+    override fun actionPerformed(anActionEvent: AnActionEvent) {
+        val project = anActionEvent.project
+        val dialog = SettingsDialog(project)
+        dialog.show()
+    }
+
+    override fun update(e: AnActionEvent?) {
+        super.update(e)
+    }
+}
